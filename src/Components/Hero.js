dbf11996263ab38c1196ed68ae083b69ca72abff
Gourav <gore.auluck@gmail.com>
2021-02-07 15:07:43 -0800

    Hero Section Media

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -3,6 +3,7 @@ import './Hero.css';
 
 import Fade from 'react-reveal/Fade';
 
+// can add delay={seconds}
 
 const Hero = () => {
     return (
@@ -11,10 +12,13 @@ const Hero = () => {
                 <Fade left>
                 <div className="image item"></div>
                 </Fade>
-                <div className="quote1 item"></div>
-                <div className="hunter-image item"></div>
-                {/* <div className="item"></div> */}
-                {/* <div className="item"></div> */}
+                <div className="quote1 item">"...the Vanguard is the best bet I've ever lost..."</div>
+                <div className="header item">
+                    <h1>Cayde-6: Hunter Vanguard, Legend</h1>
+                </div>
+                <div className="hunter-symbol item"></div>
+                {/* <div className="item">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/qopcxL2fa4g" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div> */}
             </div>
         </div>
     )
