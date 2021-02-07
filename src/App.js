e20cb2284db1faf1b9c36ef4a5f826e7ea992e54
Gourav <gore.auluck@gmail.com>
2021-02-06 18:01:36 -0800

    Updated Landing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Landing from './Components/Landing'
 
 function App() {
   return (
     <div className="App">
-        <h1>Cheese</h1>
+      <Landing />
     </div>
   );
 }
---
+++ src/Components/Landing.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Landing.css';
+
+const Landing = () => {
+    return (
+        <div className="landing-container">
+            <h1 className="title">The Man They Called Cayde</h1>
+            <div className="neon-light"></div>
+        </div>
+    )
+}
+
+export default Landing
