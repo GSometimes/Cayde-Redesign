80fdb87c8bb966ce5031b3c9b964525219b1a9da
Gourav <gore.auluck@gmail.com>
2021-02-06 22:30:57 -0800

    Hero Section Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Landing from './Components/Landing'
+import Hero from './Components/Hero'
 
 function App() {
   return (
     <div className="App">
       <Landing />
+      <Hero />
     </div>
   );
 }
--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import './Hero.css';
+
+import Fade from 'react-reveal/Fade';
+
 
 const Hero = () => {
     return (
-        <div>
-            
+        <div className="hero-container">
+            <div className="grid-container">
+                <Fade left>
+                <div className="image item"></div>
+                </Fade>
+                <div className="quote1 item"></div>
+                <div className="hunter-image item"></div>
+                {/* <div className="item"></div> */}
+                {/* <div className="item"></div> */}
+            </div>
         </div>
     )
 }
