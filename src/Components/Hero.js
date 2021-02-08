20c2e3bc8d0e3a95c99b3f07d420d7f202a59e8f
Gourav <gore.auluck@gmail.com>
2021-02-07 18:35:05 -0800

    Minor Update

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -9,16 +9,12 @@ const Hero = () => {
     return (
         <div className="hero-container">
             <div className="grid-container">
-                <Fade left>
-                <div className="image item"></div>
-                </Fade>
-                <div className="quote1 item">"...the Vanguard is the best bet I've ever lost..."</div>
                 <div className="header item">
                     <h1>Cayde-6: Hunter Vanguard, Legend</h1>
                 </div>
+                <div className="image item"></div>
                 <div className="hunter-symbol item"></div>
-                {/* <div className="item">
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/qopcxL2fa4g" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div> */}
+                <div className="quote1 item">"...the Vanguard is the best bet I've ever lost..."</div>
             </div>
         </div>
     )
